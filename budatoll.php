b1f22c5a32bfce3051f7e49a8ac0a39ed00f52c9
Peaceful <Peaceful@PEASUS.chello.hu>
2024-11-22 22:35:13 +0100

    Teljes feltöltés

--- budatoll.php
+++ budatoll.php
@@ -54,8 +54,10 @@ add_filter('wp_nav_menu_args', 'budatoll_menu_based_on_role');
 
 add_action('budatoll_cron_hook', 'budatoll_crontab');
 add_filter('cron_schedules', 'budatoll_cron_interval');
-add_action('wp_login', 'bt_log_user_login', 10, 2);
-add_action('wp_logout', 'bt_log_user_logout');
+
+add_action('wp_login', 'bt_log_user_login', 5, 2);
+add_action('clear_auth_cookie', 'bt_log_user_logout');
+add_action('wp_login_failed', 'bt_log_login_failed');
 
 add_action('user_register', 'bt_set_default_role');
 function bt_enqueue_dashicons() {
