ad7757d67d4e9771a16f6dc190948490d03d98bb
Peaceful <Peaceful@PEASUS.chello.hu>
2025-01-21 15:10:53 +0100

    Teljes feltöltés

--- budatoll.php
+++ budatoll.php
@@ -14,6 +14,11 @@ setlocale(LC_TIME, 'hu_HU.UTF-8');
 require_once plugin_dir_path(__FILE__) . 'config.inc';
 require_once plugin_dir_path(__FILE__) . 'functions.inc';
 require_once plugin_dir_path(__FILE__) . 'includes/view.inc';
+require_once plugin_dir_path(__FILE__) . 'includes/settings_event_types.inc';
+require_once plugin_dir_path(__FILE__) . 'includes/settings_logger.inc';
+require_once plugin_dir_path(__FILE__) . 'includes/settings_manage_emails.inc';
+require_once plugin_dir_path(__FILE__) . 'includes/settings_manage_players.inc';
+require_once plugin_dir_path(__FILE__) . 'includes/settings_options.inc';
 require_once plugin_dir_path(__FILE__) . 'includes/manage_payments.inc';
 require_once plugin_dir_path(__FILE__) . 'includes/payment_accounts.inc';
 require_once plugin_dir_path(__FILE__) . 'includes/events_list.inc';
@@ -23,7 +28,7 @@ require_once plugin_dir_path(__FILE__) . 'includes/trainings_calendar.inc';
 require_once plugin_dir_path(__FILE__) . 'includes/trainings_list.inc';
 require_once plugin_dir_path(__FILE__) . 'includes/scheduler.inc';
 require_once plugin_dir_path(__FILE__) . 'ajax.inc';
-require_once plugin_dir_path(__FILE__) . 'settings.inc';
+// require_once plugin_dir_path(__FILE__) . 'settings.inc';
 
 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
@@ -44,11 +49,16 @@ add_shortcode('budatoll-my-balance', 'bt_my_balance');
 add_shortcode('budatoll-rules', 'bt_rule_options');
 add_shortcode('budatoll-welcome-message', 'bt_welcome_message');
 
+add_shortcode('budatoll-settings-options', 'budatoll_setting_options');
+add_shortcode('budatoll-settings-logger', 'bt_logger_list');
+add_shortcode('budatoll-settings-event-types', 'budatoll_manage_event_types');
+add_shortcode('budatoll-settings-emails', 'budatoll_manage_emails');
+add_shortcode('budatoll-settings-players', 'budatoll_manage_players');
+
 register_activation_hook(__FILE__, 'budatoll_activated');
 register_deactivation_hook(__FILE__, 'budatoll_deactivated');
 
 add_action('wp_enqueue_scripts', 'budatoll_scripts');
-add_action('admin_enqueue_scripts', 'budatoll_admin_styles');
 add_action('template_redirect', 'budatoll_is_page_allowed');
 
 add_action('wp_login', 'budatoll_redirect_after_login');
@@ -81,7 +91,7 @@ function budatoll_scripts() {
     global $post;
     wp_enqueue_style('budatoll-jquery-style', plugins_url('jquery-ui/jquery-ui.css', __FILE__));
     wp_enqueue_style('budatoll-fc-style', plugins_url('css/fullcalendar.css', __FILE__));
-    wp_enqueue_style('budatoll-style', plugins_url('css/budatoll.css', __FILE__), [],'1.1.1', false);
+    wp_enqueue_style('budatoll-style', plugins_url('css/budatoll.css', __FILE__), [], '1.1.1', false);
     wp_enqueue_style('budatoll-mobile-style', plugins_url('css/mobile.css', __FILE__));
 
     wp_enqueue_script('budatoll-fc', plugins_url('fullcalendar/index.global.min.js', __FILE__), array('jquery'), false, false);
